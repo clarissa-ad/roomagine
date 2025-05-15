02d8af5f32bdefb9578c3ab91445212727ef579e
Kalvin <kalvinsupriadi3@gmail.com>
2025-05-15 20:11:40 +0700

    first commit

--- src/pages/InputDisplay.jsx
+++ src/pages/InputDisplay.jsx
@@ -42,7 +42,7 @@ export function InputDisplay() {
 
         try {
             const response = await axios.post(
-                "https://nominally-picked-grubworm.ngrok-free.app/webhook-test/post-image",
+                "https://primary-production-78161.up.railway.app/webhook-test/post-image",
                 formData,
                 {
                     headers: {
